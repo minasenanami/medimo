5d737ed706e479082b3ebf9cf22b10a6886c355d
僚 <>
2021-07-30 11:39:08 +0900

    RSpecの初期設定を修正

--- config/application.rb
+++ config/application.rb
@@ -37,15 +37,12 @@ module Medimo
 
     config.generators do |g|
       g.test_framework :rspec,
-                        fixtures: true,
-                        view_spec: false,
-                        routing_specs: false,
-                        helper_specs: false,
-                        controller_specs: false,
-                        request_specs: true
+                       fixtures: true,
+                       view_spec: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
     end
-
-    config.api_only = true
-    config.middleware.use ActionDispatch::Flash
   end
 end
