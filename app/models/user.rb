96ec1c2880018b5e9ff7e4e4877ea3700f2da8ca
僚 <>
2021-07-30 15:42:01 +0900

    UserモデルでActiveStrageを使用できるように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   validates :name, presence: true
   validates :profile, length: { maximum: 200 }
 end
