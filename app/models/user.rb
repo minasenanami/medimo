7debef1436439796284cf8ec186f5a23ffa3573f
僚 <>
2021-07-31 13:36:15 +0900

    UserモデルにLikeとKeepモデルの関連付け情報を付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
   has_many :articles, dependent: :destroy
+
   has_many :likes, dependent: :destroy
+  has_many :liked_articles, through: :likes, source: :article
+
+  has_many :keeps, dependent: :destroy
+  has_many :kept_articles, through: :keeps, source: :article
 
   validates :name, presence: true
   validates :profile, length: { maximum: 200 }
