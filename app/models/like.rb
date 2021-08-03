bb2e2085ec5b9b2d325adb8b26c406cb4f715660
僚 <>
2021-08-03 13:30:24 +0900

    いいね数の表示実装

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :article
+  belongs_to :article, counter_cache: :likes_count
 
   validates :user_id, uniqueness: {
     scope: :article_id,
