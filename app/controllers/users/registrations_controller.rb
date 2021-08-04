505ff045efd68834eaf3d481e6a5a1c8110d2fcf
僚 <>
2021-08-04 19:21:31 +0900

    指摘内容確認し変更不要のため警告対象部分を除外

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: %i[update destroy]
+  before_action :ensure_normal_user, only: %i[update destroy] # rubocop:disable Rails/LexicallyScopedActionFilter
+
   def ensure_normal_user
     if resource.email == "guest@example.com"
       redirect_to root_path, alert: "ゲストユーザーは更新・削除はできません。"
