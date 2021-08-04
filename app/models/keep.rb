47141648c4af5148e81604001ae2dc417fa66811
僚 <>
2021-08-04 13:20:58 +0900

    保存機能のカウント表示の実装

--- app/models/keep.rb
+++ app/models/keep.rb
@@ -2,6 +2,8 @@ class Keep < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
+  belongs_to :article, counter_cache: :keeps_count
+
   validates :user_id, uniqueness: {
     scope: :article_id,
     message: "は同じ投稿に2回以上いいねはできません",
