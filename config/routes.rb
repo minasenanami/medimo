ea49dce7626ea65690bd0eb3ade7c3bc5ee7ac39
僚 <>
2021-08-04 19:11:45 +0900

    ゲストログイン使用の為routesの一部変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root "articles#index"
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   resources :articles do
     resource :likes, only: [:create, :destroy]
     resource :keeps, only: [:create, :destroy]
