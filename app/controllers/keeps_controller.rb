c6aa0ba970865098d19c0cabecc8cb1d293d91b5
僚 <>
2021-08-04 13:07:17 +0900

    保存機能の非同期処理を実装

--- app/controllers/keeps_controller.rb
+++ app/controllers/keeps_controller.rb
@@ -1,11 +1,11 @@
 class KeepsController < ApplicationController
   def create
     current_user.keeps.create!(article_id: params[:article_id])
-    redirect_back(fallback_location: root_path)
+    @article = Article.find(params[:article_id])
   end
 
   def destroy
     current_user.keeps.find_by(article_id: params[:article_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @article = Article.find(params[:article_id])
   end
 end
