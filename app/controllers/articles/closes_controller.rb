71054c83b7cf46c27928c6dd9ba7b8ac6c5e2c97
僚 <>
2021-08-07 11:55:26 +0900

    下書き・非公開記事への手打ち入力対策を実装

--- app/controllers/articles/closes_controller.rb
+++ app/controllers/articles/closes_controller.rb
@@ -1,17 +1,19 @@
 class Articles::ClosesController < ApplicationController
-  before_action :correct_user, only: [:show]
+  before_action :closed_owner, only: [:show]
   def index
     @closed_articles = current_user.articles.closed.order(updated_at: :desc)
   end
 
   def show
-    # @article = current_user.articles.closed.find(params[:id])
+    @article = current_user.articles.closed.find(params[:id])
   end
 
-  def correct_user
-    @article = current_user.articles.closed.find_by(id: params[:id])
-    unless @article
-      redirect_to root_path
+  private
+
+    def closed_owner
+      @article = current_user.articles.closed.find_by(id: params[:id])
+      unless @article
+        redirect_to root_path, alert: "指定した記事は閲覧できません"
+      end
     end
-  end
 end
--- app/controllers/articles/drafts_controller.rb
+++ app/controllers/articles/drafts_controller.rb
@@ -1,10 +1,20 @@
 class Articles::DraftsController < ApplicationController
-before_action :correct_user, only: [:show]
+  before_action :draft_owner, only: [:show]
 
   def index
     @draft_articles = current_user.articles.draft.order(updated_at: :desc)
   end
 
   def show
+    @article = current_user.articles.draft.find(params[:id])
   end
+
+  private
+
+    def draft_owner
+      @article = current_user.articles.draft.find_by(id: params[:id])
+      unless @article
+        redirect_to root_path, alert: "指定した記事は閲覧できません"
+      end
+    end
 end
