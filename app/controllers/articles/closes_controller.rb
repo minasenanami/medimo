3d2d84e23915b74c34e11970f7d434ea6dc2ef43
僚 <>
2021-08-07 11:03:10 +0900

    非公開記事へのurl手打対策を実装

--- app/controllers/articles/closes_controller.rb
+++ app/controllers/articles/closes_controller.rb
@@ -1,9 +1,17 @@
 class Articles::ClosesController < ApplicationController
+  before_action :correct_user, only: [:show]
   def index
     @closed_articles = current_user.articles.closed.order(updated_at: :desc)
   end
 
   def show
-    @article = current_user.articles.closed.find(params[:id])
+    # @article = current_user.articles.closed.find(params[:id])
+  end
+
+  def correct_user
+    @article = current_user.articles.closed.find_by(id: params[:id])
+    unless @article
+      redirect_to root_path
+    end
   end
 end
