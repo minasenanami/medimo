8687c8e55d48b020e94066d6840fdbc97123d731
僚 <>
2021-08-08 18:36:38 +0900

    保存記事一覧ページの実装

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,9 @@
 class MypageController < ApplicationController
+  # 保存した記事一覧のページ
+  def index
+    @articles = current_user.kept_articles.order(created_at: :desc).page(params[:page]).per(PER_PAGE)
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.published.order(updated_at: :desc).page(params[:page]).per(PER_PAGE)
