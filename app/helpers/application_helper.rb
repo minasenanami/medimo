0c59bbaa194e187ed7c05d29ed83553fcfde325a
僚 <>
2021-08-10 15:22:00 +0900

    自作の作成・更新日時のフォーマットの記述をhelperを使用し実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
   def author?(article)
     article.user == current_user
   end
+
+  def creation_date(article)
+    l article.created_at, format: :medium
+  end
+
+  def update_data(article)
+    l article.updated_at, format: :medium
+  end
 end
