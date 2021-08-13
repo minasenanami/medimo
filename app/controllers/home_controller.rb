97139a40bc807540921a431a7bd40e4a7e517c79
僚 <>
2021-08-13 10:56:26 +0900

    rootページの表示をcreated_at順に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @articles = Article.published.includes(:tag_maps, :tags, :keeps,
-                                           user: { avatar_attachment: :blob }).order(updated_at: :desc).page(params[:page]).per(PER_PAGE)
+                                           user: { avatar_attachment: :blob }).order(created_at: :desc).page(params[:page]).per(PER_PAGE)
     @q = Article.published.ransack(params[:q])
   end
 end
