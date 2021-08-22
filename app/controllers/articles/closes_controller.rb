219671f5357853bbfdda4b5c38096a92785934af
僚 <>
2021-08-22 14:17:03 +0900

    非公開記事の削除機能実装

--- app/controllers/articles/closes_controller.rb
+++ app/controllers/articles/closes_controller.rb
@@ -1,14 +1,20 @@
 class Articles::ClosesController < ApplicationController
-  before_action :closed_owner, only: [:show]
-  before_action :authenticate_user!, only: %i[index show]
+  before_action :closed_owner, only: [:show, :destroy]
+  before_action :authenticate_user!, only: [:index, :show]
   def index
     @closed_articles = current_user.articles.closed.order(updated_at: :desc)
   end
 
   def show
-    @article = current_user.articles.closed.find(params[:id])
     @article_tags = @article.tags
   end
+
+  def destroy
+    @article.destroy!
+    @article.images.purge
+    redirect_to mypage_path(current_user)
+  end
+
 
   private
 
