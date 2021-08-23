1fbdb396097032bb81511ce85bf0e9d1946a6082
僚 <>
2021-08-23 11:24:33 +0900

    全ページの表示レイアウト幅を変更し統一

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
-  def max_width
-    if controller_name == "articles" && action_name == "show"
-      "mw-md"
-    elsif devise_controller?
-      "mw-sm"
-    else
-      "mw-xl"
-    end
-  end
-
   def author?(article)
     article.user == current_user
   end
