19c1191f2aa4c1b55762da0372c7434f8fd012ea
僚 <>
2021-08-27 14:45:04 +0900

    タグのモデルテストを実装

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "タグが入力されている時" do
+    let(:tag) { build(:tag) }
+    it "タグが生成できる" do
+      expect(tag).to be_valid
+    end
+  end
+
+  context "タグが空欄のとき" do
+    let(:tag) { build(:tag, tag_name: nil) }
+    it "保存に失敗する" do
+      expect(tag).to be_invalid
+      expect(tag.errors.details[:tag_name][0][:error]).to eq :blank
+    end
+  end
 end
