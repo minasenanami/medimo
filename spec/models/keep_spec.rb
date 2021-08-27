a567c700166fd4f00c93feeffb80a2f75d71a9de
僚 <>
2021-08-27 14:43:45 +0900

    保存機能のモデルテストを実装

--- spec/models/keep_spec.rb
+++ spec/models/keep_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Keep, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "データが条件を満たすとき" do
+    let(:keep) { build(:keep) }
+    it "保存できる" do
+      expect(keep).to be_valid
+    end
+  end
+
+  context "article_id がすでに存在するとき" do
+    let(:user) { create(:user) }
+    let(:article) { create(:article, user: user) }
+
+    let!(:first_keep) { create(:keep, user: user, article: article) }
+    let(:second_keep) { build(:keep, user: user, article: article) }
+    it "保存に失敗する" do
+      expect(second_keep).to be_invalid
+      expect(second_keep.errors.details[:user_id][0][:error]).to eq :taken
+    end
+  end
 end
