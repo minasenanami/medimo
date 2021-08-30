118a74a3fb7c5931bff21347b4043bb95e5cfba8
僚 <>
2021-08-30 16:56:43 +0900

    モデルテストの実装

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,15 +2,16 @@ require "rails_helper"
 
 RSpec.describe Article, type: :model do
   describe "正常系" do
-    context "タイトルと本文と公開レベルが指定されているとき" do
+    context "タイトルと本文が存在するとき" do
       let(:article) { build(:article) }
       it "公開状態で記事が作成される" do
-        expect(article).to be_valid
+        expect(article.content.body.present?).to eq true
         expect(article.status).to eq "published"
+        expect(article).to be_valid
       end
     end
 
-    context "statusがpublishedのとき" do
+    context "必要情報が記載されstatusがpublishedのとき" do
       let(:article) { build(:article, :published) }
       it "公開状態で記事を作成できる" do
         expect(article).to be_valid
@@ -33,6 +34,27 @@ RSpec.describe Article, type: :model do
         expect(article.status).to eq "closed"
       end
     end
+
+    context "statusがdraftのとき" do
+      context "contentがnilでも" do
+        let(:article) { build(:article, :draft, content: nil) }
+        it "下書き記事を作成できる" do
+          expect(article).to be_valid
+          expect(article.status).to eq "draft"
+        end
+      end
+    end
+
+    context "statusがclosedのとき" do
+      context "contentがnilでも" do
+        let(:article) { build(:article, :closed, content: nil) }
+        it "非公開記事を作成できる" do
+          expect(article).to be_valid
+          expect(article.status).to eq "closed"
+        end
+      end
+    end
+
   end
 
   describe "異常系" do
@@ -44,11 +66,20 @@ RSpec.describe Article, type: :model do
       end
     end
 
-    context "contentが無いとき" do
-      let(:article) { build(:article, content: nil) }
-      it "記事の作成に失敗する" do
+    context "公開記事のcontentが無いとき" do
+      let(:article) { build(:article, :published, content: nil) }
+      it "公開記事の作成に失敗する" do
+        expect(article.content.body.blank?).to eq true
         expect(article).to be_invalid
         expect(article.errors.details[:content][0][:error]).to eq :blank
+      end
+    end
+
+    context "statusがないとき" do
+      let(:article) { build(:article, status: nil) }
+      it "記事の作成に失敗する" do
+        expect(article).to be_invalid
+        expect(article.errors.details[:status][0][:error]).to eq :blank
       end
     end
   end
