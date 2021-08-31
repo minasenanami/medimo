92ebc97c7ed808d1ef733f42c7aa78bd6fdf5370
僚 <>
2021-08-31 15:15:02 +0900

    password強化のためゲストユーザーのみ先に作る実装に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
+require "csv"
+
 User.destroy_all
 Article.destroy_all
 Tag.destroy_all
 TagMap.destroy_all
 
-require "csv"
+guest_user_profile = <<~EOS
+機能制限中につき以下は行えません
+  ・プロフィール変更
+  ・登録内容の変更
+  ・ユーザーの削除
+EOS
+
+NAME = "ゲストユーザー"
+EMAIL = "guest@example.com"
+PASSWORD = SecureRandom.urlsafe_base64
+PROFILE = guest_user_profile
+
+puts "ゲストユーザーを作成します"
+User.create!(name: NAME, email: EMAIL, password: PASSWORD, profile: PROFILE )
+puts "ゲストユーザーが作成されました"
+
 
 puts "ユーザーデータのインポートを開始します"
 
@@ -16,20 +33,3 @@ User.create!(user_data_list)
 user1, user2, user3, user4, user5, user6 = User.all
 
 puts "ユーザーデータのインポートに成功しました"
-
-def import_article(path, user)
-  list = []
-  CSV.foreach("db/csv_data/article_data/#{path}", headers: true) do |row|
-    list << { title: row["title"], content: row["content"], status: row["status"].to_i, user: user }
-  end
-  Article.create!(list)
-end
-
-puts "記事データのインポートを開始します"
-import_article("user1_article_data.csv", user1)
-import_article("user2_article_data.csv", user2)
-import_article("user3_article_data.csv", user3)
-import_article("user4_article_data.csv", user4)
-import_article("user5_article_data.csv", user5)
-
-puts "記事データのインポートが完了しました"
